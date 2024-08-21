3e6708c7bdc663bfcd36d242495b04cadf7094b0
Facelles <122049357+Facelles@users.noreply.github.com>
2024-08-21 21:41:38 +0300

    nodejsWithoutTypeScript

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 import express from 'express'
-import { twitRouter } from './src/twit/twit.controller.js'
 import dotenv from 'dotenv'
+import { twitRouter } from './src/twit/twit.controller.js'
+import { authMiddleware } from './src/auth.middleware.js'
 
 dotenv.config()
 
 const app = express()
+
+// app.use(authMiddleware)
 
 async function main(){
     app.use(express.json())
 
     app.use('/api/twits', twitRouter)
 
+        app.get('/error', (req, res) => {
+        throw new Error('This is a test error! =-=')
+    });
+
     app.all('*', (req, res) => {
         res.status(404).json({ message: 'Not found' })
-    })
+    });
+
+    app.use((err,req,res,next) => {
+        console.error(err.stack)
+        res.status(500).send('Something wrong! ')
+    });
 
     app.listen(process.env.PORT || 4200, () => {
         console.log('Server is running on port 4200')
     })
 }
 
-main()
+main() 
--- src/twit/twit.controller.js
+++ src/twit/twit.controller.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { TwitService } from './twit.service.js'
+import { authMiddleware } from '../auth.middleware.js'
 
 const router = Router()
 
 const twistService = new TwitService()
 
-router.post('/', (req, res) => {
+router.post('/', authMiddleware ,(req, res) => {
+    // console.log(req.headers)
     if(req.body?.text?.length < 1){
         res.status(400).send({message: 'Text is required'})
     }
